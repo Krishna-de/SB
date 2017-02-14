b2e33cc96e7f75580134dc8d8b2ef0bd312b4821
Krishna-de <Krishna@Krishnas-MacBook-Air.local>
2017-02-14 02:55:03 +0100

    Second change

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -5,28 +5,28 @@ var demoSuggestions = {
 	posts: [
 		{
 			title: 'Free pizza at club meetings',
-			upvotes: 15,
+			upvotes: 0,
 			comments: [],
 			id: 0
 		},
 
 		{
 			title: 'End all club emails with Laffy Taffy jokes',
-			upvotes: 9,
+			upvotes: 0,
 			comments: [],
 			id: 1
 		},
 
 		{
 			title: 'Retrofit water fountain with Gatorade',
-			upvotes: 7,
+			upvotes: 0,
 			comments: [],
 			id: 2
 		},
 
 		{
 			title: 'Sing Bon Jovi\'s "Living on a Prayer" halfway through meetings',
-			upvotes: 3,
+			upvotes: 0,
 			comments: [],
 			id: 3
 		},
